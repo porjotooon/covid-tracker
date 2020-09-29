7e28d602de616980e897f722dcf7fbda5de3269c
Nafis Hasnain <nafishasnain0@gmail.com>
2020-09-29 01:14:44 -0400

    Removed line chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import styles from './Chart.module.css'
 import { fetchDailyData } from '../utils/Api'
-import { Line, Bar } from 'react-chartjs-2'
+import { Bar } from 'react-chartjs-2'
 
 const Chart = () => {
     const [dailyData, setDailyData] = useState([])
@@ -15,33 +15,9 @@ const Chart = () => {
         fetchAPI();
     }, [dailyData])
 
-    {/*if first dailyData is available, fill in line*/}
-    const lineChart = (
-        dailyData[0] ? (
-          <Line
-            data={{
-              labels: dailyData.map(({ date }) => date),
-              datasets: [{
-                data: dailyData.map((data) => data.confirmed),
-                label: 'Infected',
-                borderColor: '#3333ff',
-                fill: true,
-              }, {
-                data: dailyData.map((data) => data.deaths),
-                label: 'Deaths',
-                borderColor: 'red',
-                backgroundColor: 'rgba(255, 0, 0, 0.5)',
-                fill: true,
-              },
-              ],
-            }}
-          />
-        ) : null
-      );
-
     return (
         <div className={styles.container}>
-            {lineChart}
+
         </div>
     )
 }
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -14,12 +14,3 @@ export const fetchData = async () => {
     }
 }
 
-export const fetchDailyData = async () => {
-    try {
-      const { data } = await axios.get(`${url}/daily`);
-  
-      return data.map(({ confirmed, deaths, reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
-    } catch (error) {
-      return error;
-    }
-  };
